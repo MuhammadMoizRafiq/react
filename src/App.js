d00fb24f5084e51123a3c88c8b8f79a5afda8e81
muhammadmoizrafiq <m4moiz2000@gmail.com>
2023-02-07 17:19:04 +0500

    Work Accomplish

--- src/App.js
+++ src/App.js
@@ -1,131 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  // let a =10;
-  // let arr =[1,2,3,4,5,6,7,8,9,]
-  let mobile =[
-    {
-      name: "a30",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 2000,
-      brand: "samsung",
-    },
-    {
-      name: "note10",
-      ram: "6gb",
-      rom: "128gb",
-      camera: "50px",
-      price: 4000,
-      brand: "samsung",
-    },
-    {
-      name: "s10",
-      ram: "3gb",
-      rom: "128gb",
-      camera: "10px",
-      price: 5000,
-      brand: "samsung",
-    },
-    {
-      name: "iphone4",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone4s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 20000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone5",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone6",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "redminote10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redminote11",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10pro",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "oppo",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "Realmi",
-    },
-  ]
-  return (
+  const [todo, setTodo] = useState([]);
+  const [text,setText] = useState("");
+  const [indexNum,SetindexNum] = useState();
+  let add = () =>{
+    if(indexNum > -1){
+      todo[indexNum] = text;
+      setTodo([...todo]);
+      SetindexNum(-1);
+    }
+    else{
+      todo.push(text);
+      setTodo([...todo]);
+    }
+    setText("");
+}
+  let del = (i) =>{
+    todo.splice(i,1);
+    setTodo([...todo]);
+  }
+  let edit = (i) =>{
+    SetindexNum(i);
+    setText(todo[i]);
+  }
+ return (
     <div className="App">
       <header className="App-header">
-      <h1> Muhammamd Moiz </h1>
-        {mobile.map((x,i) => {
-          return(
-            <div key={i} className='card'>
-              <h1>{x.brand}</h1>
-              <p>{x.name}</p>
-              <p>{x.camera}</p>
-              <p>{x.ram}</p>
-              <p>{x.rom}</p>
-              <p>Rs{x.price}/-</p>
-              </div>
-          );
-        })}
+        <h1>Todo App</h1>
+         <input className='inp' onChange={(e) =>{
+          setText(e.target.value);
+         }} type="text" placeholder='Enter Text' />
+        <button className='btn' onClick={add}>Add</button>
+       {todo.map((x,i) =>{
+        return(
+          <li key={i}>
+          {x}
+          <button className='btn' onClick={() => del(i)}>Delete</button>
+          <button className='btn' onClick={() => edit(i)}>Edit</button>
+
+          </li>
+        )
+       })}
+
+
+  
        
       </header>
     </div>
